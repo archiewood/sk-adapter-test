a158f949cc1b4a6e786dd25430431d2772285c7d
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-04-08 11:57:58 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- Category Href links -->
+
+{#each Array.from({ length: 1000000 }, (_, i) => i + 1) as i}
+  <a href="/page{i}">{i}</a>
+  <br/>
+{/each}
---
+++ src/routes/[category]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { page } from '$app/stores';
+    // random number
+</script>
+
+{$page.params.category}
+
+{#each Array.from({ length: 100 }, (_, i) => i + 1) as i}
+    
+    
+    <br/>
+    {Math.floor(Math.random() * 1000000)}
+    
+{/each}
+
